efc25fb310b7ee31d45678a4c0cbf0e3aaae3e17
jessehsiu <jesse801122@gmail.com>
2014-11-02 02:05:49 +0800

    fixed multi-hands

--- leapmotion.js
+++ leapmotion.js
@@ -2,7 +2,7 @@ var Leap = require('leapjs');
 
 var controllerOptions = {enableGestures: true};
 
-var jsontosend;
+var info;
 
 Leap.loop(controllerOptions, function(frame) {
   // Body of callback function
@@ -14,30 +14,41 @@ Leap.loop(controllerOptions, function(frame) {
 	                //+ "Tools: " + frame.tools.length 
 	                //+ "Gestures: " + frame.gestures.length;
 
-	console.log(frameString);
-
+	//console.log(frameString);
+	//
 	if (frame.hands.length > 0) {
+		var palmstosend=[];
+		var fingerstosend=[];
 		for (var i = 0; i < frame.hands.length; i++) {
 			var hand = frame.hands[i];
 			
 	        var position = hand.palmPosition;
 	        var velocity = hand.palmVelocity;
 	        var direction = hand.direction;
-	        var handstosend=[];
-	        
-	        
-
 	        var fingers = hand.fingers;
+
 	        for( var j = 0 ; j<fingers.length; j++)
 	        {
-	        	var finger = fingers[j];
-	        	
-	        	handstosend.push({type: finger.type,carp:finger.carpPosition,dip:finger.dipPosition,mcp:finger.mcpPosition,pip:finger.pipPosition});
 
+	        	var finger = fingers[j];
+
+	        	fingerstosend.push({type: finger.type,carp:finger.carpPosition,dip:finger.dipPosition,mcp:finger.mcpPosition,pip:finger.pipPosition});
 	        }
-	        jsontosend = { type:hand.type, position: position, velocity: velocity, direction: direction,hands: handstosend};
-	        //console.log(jsontosend);
+
+	        palmstosend.push({type:hand.type, 
+	        					position: position, 
+	        					velocity: velocity,
+	        					direction: direction,
+	        					fingers:fingerstosend
+	        				});
 		}
+
+		info = {frameid:frame.id,frametimestamp:frame.timestamp, Hands:frame.hands.length, Fingers:frame.fingers.length,hands:palmstosend};
 	}
+	var jsontosend = JSON.stringify(info);
+	console.log(jsontosend + "\n");
+
+
+
 
 })
